fff83682da5dea9599c2a70c427d6206df68b77e
eliseven-7 <feng.yinu@northeastern.edu>
2024-11-17 00:10:32 -0800

    Fixed setup issues to connect a docker database

--- sky-common/src/main/java/com/sky/constant/MessageConstant.java
+++ sky-common/src/main/java/com/sky/constant/MessageConstant.java
@@ -4,13 +4,13 @@ package com.sky.constant;
  * 信息提示常量类
  */
 public class MessageConstant {
-    public static final String ALREADY_EXISTS = "已存在";
+    public static final String ALREADY_EXISTS = "Already exists";
 
-    public static final String PASSWORD_ERROR = "密码错误";
-    public static final String ACCOUNT_NOT_FOUND = "账号不存在";
-    public static final String ACCOUNT_LOCKED = "账号被锁定";
-    public static final String UNKNOWN_ERROR = "未知错误";
-    public static final String USER_NOT_LOGIN = "用户未登录";
+    public static final String PASSWORD_ERROR = "Wrong password";
+    public static final String ACCOUNT_NOT_FOUND = "Account not found";
+    public static final String ACCOUNT_LOCKED = "Account locked";
+    public static final String UNKNOWN_ERROR = "Unknown error";
+    public static final String USER_NOT_LOGIN = "User not login";
     public static final String CATEGORY_BE_RELATED_BY_SETMEAL = "当前分类关联了套餐,不能删除";
     public static final String CATEGORY_BE_RELATED_BY_DISH = "当前分类关联了菜品,不能删除";
     public static final String SHOPPING_CART_IS_NULL = "购物车数据为空，不能下单";
